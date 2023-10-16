0daf84a98db3b69fd073c0bc5ae7843aa674541c
Ismaile <ismailearliene@gmail.com>
2023-10-15 23:15:20 -0300

    atualizando comentarios

--- script.js
+++ script.js
@@ -30,37 +30,6 @@ $(document).ready(function () {
 });
 
 
-const teste = [{
-  nome: 'ismaile',
-  comentario: 'Ótima qualidade exelente atendimento.',
-  nome: 'ismaile',
-  comentario: 'Ótima qualidade exelente atendimento.',
-
-}, {
-  nome: 'arliene',
-  comentario: 'Ótima qualidade',
-}
-  , {
-  nome: 'thomas',
-  comentario: 'Ótima qualidade',
-}
-  , {
-  nome: 'ada',
-  comentario: 'Ótima qualidade ima qualidade ima qualidade ima qualidade ima qualidade ima qualidade s',
-}
-  , {
-  nome: 'layla',
-  comentario: 'Ótima qualidade',
-}
-
-
-
-
-]
-
-
-
-
 let inputs = document.querySelectorAll('input')
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
